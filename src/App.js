83d5a8134f1226e2e76c46db6fafe51aac02b2dc
Milan M <milanmalenak@gmail.com>
2023-08-13 20:28:40 +0200

    Added form, fetching rates from API.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,57 @@
 import './App.css';
+import currencyService from './services/currencies'
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
-  const [amount, setAmount] = useState('')
-  const [currency, setCurrency] = useState('')
+  const defaultCurrency = 'EUR'
+  var convertedAmount = 0
 
+  const [amount, setAmount] = useState('')
+  const [currency, setCurrency] = useState(defaultCurrency)
+  const [conversionObject, setConversionObject] = useState({})
+  // const [convertedAmount, setConvertedAmount] = useState(0);
+
+  useEffect(() => {
+    if (amount !== ''){
+      setTimeout(() => {
+        currencyService
+          .convert(currency, amount)
+          .then(function(object){
+            setConversionObject(object)
+          // .then(function (object) {
+          //   setConversionObject(object);
+          //   const rateForAmount = object.rates && object.rates[currency] ? object.rates[currency].rate_for_amount : 0;
+          //   setConvertedAmount(rateForAmount);
+          })
+          // .catch((error) => {
+          //   console.log(error)
+          //   setConvertedAmount(0)
+          // })
+      }, 2000)
+    }
+  }, [currency, amount])
+    
+  console.log('amount', amount)
+  console.log('currency', currency)
+  console.log('conversionObject', conversionObject)
+
+  try{
+    convertedAmount = conversionObject.rates ? conversionObject.rates[`${currency}`].rate_for_amount : 0
+  } catch (error) {
+    console.log(error)
+  }  
 
   const handleAmountChange = (event) => {
-    setAmount(event.target.value)
+    const inputAmount = event.target.value
+    if( /^\d*\.?\d*$/.test(inputAmount) ){
+      setAmount(inputAmount)
+    }
+  }
+
+  const handleCurrencyChange = (event) => {
+    setCurrency(event.target.value)
   }
 
   const addExchange = () => {
@@ -27,22 +69,30 @@ function App() {
         <form onSubmit={addExchange} >
           <div className="App-form-amount">
             <div>
-              amount: <input
+              amount <input
                   type="text"
-                  inputmode="numeric"
+                  inputMode="numeric"
                   value={amount}
                   onChange={handleAmountChange}
               />
             </div>
             <div>
-              <select id="currencies">
-                <option value="EUR">EUR</option>
-                <option value="CZK">CZK</option>
+              <select
+                id="currencies"
+                name="currencies"
+                defaultValue={defaultCurrency}
+                onChange={handleCurrencyChange}
+              >
+                <option value={defaultCurrency} >{defaultCurrency}</option>
+                <option value='GBP' >GBP</option>
               </select>
             </div>
           </div>
           <div>
-              amount in USD: {currency}
+              selected values: {amount} {currency}
+          </div>
+          <div>
+              amount in USD: {convertedAmount}
           </div>
           <div>
             <button type='submit' >Save</button>
