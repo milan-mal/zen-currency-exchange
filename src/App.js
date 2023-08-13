2f8feed45e6ae2d978402f07f7bb4b5bdae0ae73
milan-mal <milanmalenak@gmail.com>
2023-08-13 15:06:08 +0200

    added basic structure of the form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
 function App() {
+
+  const [amount, setAmount] = useState('')
+  const [currency, setCurrency] = useState('')
+
+
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value)
+  }
+
+  const addExchange = () => {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Currency exchange
+        </h1>
       </header>
+      <div className="App-body">
+        <form onSubmit={addExchange} >
+          <div className="App-form-amount">
+            <div>
+              amount: <input
+                  type="text"
+                  inputmode="numeric"
+                  value={amount}
+                  onChange={handleAmountChange}
+              />
+            </div>
+            <div>
+              <select id="currencies">
+                <option value="EUR">EUR</option>
+                <option value="CZK">CZK</option>
+              </select>
+            </div>
+          </div>
+          <div>
+              amount in USD: {currency}
+          </div>
+          <div>
+            <button type='submit' >Save</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/services/currencies.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const baseUrl = 'https://api.getgeoapi.com/v2/currency/convert?api_key=7f0c4c50779df32ed52f8d658bd6cd38a8ce671c'
+
+const convert = (from, to, amount) => {
+}
+
+export default { convert }
